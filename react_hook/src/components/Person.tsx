9fad74d4b07397e94b522c6c357038daf5786df5
marvelopers <marvelopers@gmail.com>
2021-07-05 16:41:00 +0900

    useCallback 적용

--- react_hook/src/components/Person.tsx
+++ react_hook/src/components/Person.tsx
@@ -1,21 +1,32 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, useCallback } from 'react';
 import { Text, View, Image, Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Colors } from 'react-native-paper';
 import { styles } from './Person.style';
 import Avatar from '../components/Avatar';
 import IconText from '../components/IconText';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/ko';
 import * as Data from '../data';
 
 export type PersonProps = {
   person: Data.IPerson;
 };
 
-const handlePressAvatar = () => Alert.alert('Avatar');
-const handlePressDelete = () => Alert.alert('Delete');
-const handlePressCountIcon = (name: string) => () => Alert.alert(`${name} Pressed`);
+dayjs.locale('ko');
+dayjs.extend(relativeTime);
 
 const Person = ({ person }: PersonProps) => {
+  const handlePressAvatar = useCallback(() => Alert.alert('Avatar'), []);
+  const handlePressDelete = useCallback(() => Alert.alert('Delete'), []);
+  const handlePressCountIcon = useCallback(
+    (name: string) => () => Alert.alert(`${name} Pressed`),
+    [],
+  );
+
+  const timeToAgo = dayjs(person.createDate).fromNow();
+
   return (
     <View style={styles.view}>
       <View style={styles.leftView}>
@@ -25,7 +36,7 @@ const Person = ({ person }: PersonProps) => {
         <Text style={styles.name}>{person.name}</Text>
         <Text style={styles.email}>{person.email}</Text>
         <View>
-          <Text>??시간 전</Text>
+          <Text style={styles.text}>{timeToAgo}</Text>
           <Icon
             name="trash-can-outline"
             size={26}
