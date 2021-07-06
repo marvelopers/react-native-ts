72a00174f40806e21ae4f77374b94b1c2ccc9ad9
marvelopers <marvelopers@gmail.com>
2021-07-06 10:45:54 +0900

    object로 상태관리하기

--- react_hook/App.tsx
+++ react_hook/App.tsx
@@ -1,31 +1,66 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text, Dimensions, ScrollView } from 'react-native';
-import { useClock } from './src/hooks/useClock';
-import Cache from './src/screens/Cache';
-import Memo from './src/screens/Memo';
-import Fibo from './src/screens/Fibo';
+import React, { useMemo } from 'react';
+import type FC from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  Dimensions,
+  ScrollView,
+  View,
+  FlatList,
+} from 'react-native';
+import PersonUsingValueState from './src/screens/person/PersonUsingValueState';
+import PersonUsingObjectState from './src/screens/person/PersonUsingObjectState';
+import PersonUsingPassingState from './src/screens/person/PersonUsingPassingState';
+import { Colors } from 'react-native-paper';
+import * as Data from './src/data';
+
+type PersonInfomation = {
+  title: string;
+  Component: FC<any>;
+};
 
 const { width } = Dimensions.get('window');
-const numberOfComponents = 3;
+
+const PersonInfomations: PersonInfomation[] = [
+  { title: 'PersonUsingValueState', Component: PersonUsingValueState },
+  { title: 'PersonUsingObjectState', Component: PersonUsingObjectState },
+  { title: 'PersonUsingPassingState', Component: PersonUsingPassingState },
+];
+
+const numberOfComponents = PersonInfomations.length;
 
 const App = () => {
-  const time = useClock();
+  const people = useMemo(() => Data.makeArray(10).map(Data.createRandomPerson), []);
+  const children = useMemo(
+    () =>
+      PersonInfomations.map(({ title, Component }) => (
+        <View style={{ flex: 1 }} key={title}>
+          <Text style={styles.text}>{title}</Text>
+          <FlatList
+            data={people}
+            renderItem={({ item }) => <Component person={item} />}
+            keyExtractor={(item, index) => item.id}
+            ItemSeparatorComponent={() => <View style={styles.itemSeparator} />}
+          />
+        </View>
+      )),
+    [],
+  );
   return (
-    <SafeAreaView>
-      <ScrollView horizontal contentContainerStyle={styles.contentContainerStyle}>
-        <Cache />
-        <Memo />
-        <Fibo />
+    <SafeAreaView style={styles.safeAreaView}>
+      <ScrollView horizontal contentContainerStyle={styles.horizontalScrollView}>
+        {children}
       </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  safeAreaView: {
-    flex: 1,
-  },
-  contentContainerStyle: { width: width * numberOfComponents },
+  safeAreaView: { flex: 1 },
+  itemSeparator: { borderWidth: 1, borderColor: Colors.grey500 },
+  horizontalScrollView: { width: width * numberOfComponents },
+  text: { fontSize: 24, textAlign: 'center' },
 });
 
 export default App;
---
+++ react_hook/src/screens/person/PersonUsingObjectState.tsx
@@ -0,0 +1,101 @@
+import React, { useCallback, useState } from 'react';
+import { Text, View, Image, Alert } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Colors } from 'react-native-paper';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/ko';
+import Avatar from '../../components/Avatar';
+import IconText from '../../components/IconText';
+import { styles } from '../../components/Person.style';
+import * as Data from '../../data';
+
+dayjs.locale('ko');
+dayjs.extend(relativeTime);
+
+export type PersonProps = {
+  person: Data.IPerson;
+};
+
+// export type IconType = 'comment' | 'retweet' | 'heart';
+export enum IconType {
+  COMMENT = 'comment',
+  RETWEET = 'retweet',
+  HEART = 'heart',
+}
+
+const PersonUsingValueState = ({ person: initialPerson }: PersonProps) => {
+  const [person, setPerson] = useState<Data.IPerson>({
+    ...initialPerson,
+    counts: { comment: 0, retweet: 0, heart: 0 },
+  });
+
+  const handlePressAvatar = useCallback(() => Alert.alert('Avatar'), []);
+  const handlePressDelete = useCallback(() => Alert.alert('Delete'), []);
+  const handlePressCountIcon = useCallback(
+    (iconType: IconType) => () =>
+      setPerson(person => ({
+        ...person,
+        counts: { ...person.counts, iconType: person.counts[iconType] + 1 },
+      })),
+    [],
+  );
+
+  const timeToAgo = dayjs(person.createDate).fromNow();
+
+  return (
+    <View style={styles.view}>
+      <View style={styles.leftView}>
+        <Avatar style={styles.avatar} uri={person.avatar} size={50} onPress={handlePressAvatar} />
+      </View>
+      <View style={styles.rightView}>
+        <Text style={styles.name}>{person.name}</Text>
+        <Text style={styles.email}>{person.email}</Text>
+        <View>
+          <Text style={styles.text}>{timeToAgo}</Text>
+          <Icon
+            name="trash-can-outline"
+            size={26}
+            color={Colors.lightBlue500}
+            onPress={handlePressDelete}
+          />
+        </View>
+        <Text numberOfLines={3} ellipsizeMode="tail" style={[styles.text, styles.comments]}>
+          {person.comments}
+        </Text>
+        <Image style={styles.image} source={{ uri: person.image }} />
+        <View style={styles.countView}>
+          <IconText
+            viewStyle={styles.touchableIcon}
+            onPress={handlePressCountIcon(IconType.COMMENT)}
+            name="comment"
+            size={24}
+            color={Colors.blue500}
+            textStyle={styles.iconText}
+            text={person.counts.comment}
+          />
+          <IconText
+            viewStyle={styles.touchableIcon}
+            onPress={handlePressCountIcon(IconType.RETWEET)}
+            name="retweet"
+            size={24}
+            color={Colors.purple500}
+            textStyle={styles.iconText}
+            text={person.counts.retweet}
+          />
+          <IconText
+            viewStyle={styles.touchableIcon}
+            onPress={handlePressCountIcon(IconType.HEART)}
+            name="heart"
+            size={24}
+            color={Colors.red500}
+            textStyle={styles.iconText}
+            text={person.counts.heart}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default PersonUsingValueState;
