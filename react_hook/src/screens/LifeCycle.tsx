4daa7721bd7350d8c57f6fb8c0c1d880fc97bcf0
marvelopers <marvelopers@gmail.com>
2021-07-18 23:09:42 +0900

    LayoutChangeEvent 알아보기

--- react_hook/src/screens/LifeCycle.tsx
+++ react_hook/src/screens/LifeCycle.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useEffect, useLayoutEffect, useCallback } from 'react';
+import { Platform, LayoutChangeEvent, View, Text, StyleSheet } from 'react-native';
+import { Colors } from 'react-native-paper';
 
 const LifeCycle = () => {
-  return <div>LifeCycle</div>;
+  const handleLayout = useCallback((e: LayoutChangeEvent) => {
+    const { layout } = e.nativeEvent;
+    console.log(Platform.OS, 'handleLayout called', layout);
+  }, []);
+
+  useEffect(() => {
+    console.log(Platform.OS, 'useEffect called');
+    return () => console.log(Platform.OS, 'useEffect finish');
+  }, []);
+
+  useLayoutEffect(() => {
+    console.log(Platform.OS, 'useLayout called');
+    return () => console.log(Platform.OS, 'useLayout finish');
+  }, []);
+
+  console.log(Platform.OS, 'render start');
+
+  return (
+    <View onLayout={handleLayout} style={styles.view}>
+      <Text style={styles.title}>LifeCycle</Text>
+    </View>
+  );
 };
 
+const styles = StyleSheet.create({
+  view: { flex: 1, alignItems: 'center', backgroundColor: Colors.lightBlue500 },
+  title: { fontSize: 30, fontWeight: '600' },
+});
 export default LifeCycle;
