714823a75a2a517365d2c145d26d83bf517019ed
marvelopers <marvelopers@gmail.com>
2021-07-19 22:13:20 +0900

    Timer 구현

--- react_hook/src/screens/Timer.tsx
+++ react_hook/src/screens/Timer.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import { Colors, ActivityIndicator } from 'react-native-paper';
 
 const Timer = () => {
-  return <div>Timer</div>;
+  const [loading, setLoading] = useState(true);
+  const toggleLoading = useCallback(() => setLoading(loading => !loading), []);
+
+  useEffect(() => {
+    const id = setTimeout(() => setLoading(false), []);
+    return () => clearTimeout(id);
+  }, [loading]);
+  return (
+    <View>
+      <Text style={styles.title}>Timer</Text>
+      <Text>loading: {loading.toString()}</Text>
+      <Button onPress={toggleLoading} title={loading ? 'stop loading' : 'start loading'} />
+      {loading && <ActivityIndicator size="large" color={Colors.deepPurple700} />}
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  view: { flex: 1, alignItems: 'center', backgroundColor: Colors.yellow300 },
+  title: { fontSize: 20, fontWeight: '600' },
+});
 
 export default Timer;
