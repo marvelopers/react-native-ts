5808d8ccbdf88b521cdb9d0574e1be92b5741ab2
marvelopers <marvelopers@gmail.com>
2021-07-25 00:08:15 +0900

    react context를 사용한 다크모드 구현 프로젝트 기본 설정

---
+++ darkModeUseContext/.prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  printWidth: 100,
+  semi: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+  singleQuote: true,
+  proseWrap: "always",
+  arrowParens: "avoid"
+};
